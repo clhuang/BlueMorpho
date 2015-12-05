953261deb2ecb5d7a8c6daa32c0a405cb597b598
Brian Shimanuki <bshimanuki@mit.edu>
2015-12-05 14:24:59 -0500

    objective typo, shell tab completion

--- pysrc/__main__.py
+++ pysrc/__main__.py
@@ -1,7 +1,13 @@
 from __init__ import *
+import rlcompleter
+import readline
+
+readline.parse_and_bind('tab: complete')
 
 if __name__ == '__main__':
     # while True:
         # word = input("Input word: ")
         # pprint.pprint(en_morpho.getParentsFeatures(word))
-    pprint.pprint(en_morpho.genTrainingData())
+    train = en_morpho.genTrainingData()
+    pprint.pprint(train)
+    # optimize_weights(*train)
